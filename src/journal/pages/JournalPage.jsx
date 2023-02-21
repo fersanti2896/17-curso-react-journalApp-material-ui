08cfed5665cdb25454b728eb52b9a0216f2eaae7
Fernando Nicolás <fersanti2896@gmail.com>
2023-02-21 16:01:34 -0600

    Diseño en JournalPage.jsx

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,14 @@
 import { MailOutline } from '@mui/icons-material';
 import { Typography } from '@mui/material';
+import { JournalLayout } from '../layout/JournalLayout';
 
 export const JournalPage = () => {
     return (
         <>
-            <Typography variant='h1'>Journal Page</Typography>
-            <MailOutline />
+            <JournalLayout>
+                <Typography> nulla do labore pariatur nisi ut ut aliquip adipisicing
+                     adipisicing officia ex.</Typography>
+            </JournalLayout>
         </>
     )
 }
