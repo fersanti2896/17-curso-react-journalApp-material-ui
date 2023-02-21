faec2f8c65f52952f118a9a210ca7fff44c02577
Fernando Nicolás <fersanti2896@gmail.com>
2023-02-21 15:00:38 -0600

    Diseño de RegisterPage.jsx

--- src/auth/layout/AuthLayout.jsx
+++ src/auth/layout/AuthLayout.jsx
@@ -13,7 +13,7 @@ export const AuthLayout = ({ children, title = '' }) => {
                     <Grid item
                           className='box-shadow'
                           xs={ 3 }
-                          sx={{ backgroundColor: 'white', borderRadius:2 , padding: 3, width: { sm: 450 } }} >
+                          sx={{ backgroundColor: 'white', borderRadius:2 , padding: 4, width: { sm: 450 } }} >
                             <Typography variant='h5' sx={{ mb: 1 }}>{ title }</Typography>
 
                             { children }
--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -6,7 +6,7 @@ import { AuthLayout } from '../layout/AuthLayout';
 export const LoginPage = () => {
     return (
         <>
-           <AuthLayout title='Login'>
+           <AuthLayout title='Iniciar Sesión'>
             <form>
                     <Grid container>
                         <Grid item sx={{ mb: 2 }} xs={ 12 } >
@@ -39,6 +39,7 @@ export const LoginPage = () => {
                         </Grid>
 
                         <Grid container direction='row' justifyContent='end'>
+                            <Typography sx={{ mr: 1 }}>¿No tienes cuenta?</Typography>
                             <Link color='inherit' 
                                     component={ RouterLink } 
                                     to='/auth/register'>
