775708501635df51e473afdd6d9dfb13e039628e
Fernando Nicolás <fersanti2896@gmail.com>
2023-02-21 18:15:36 -0600

    Navbar y Sidebar.jsx

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,17 +1,21 @@
-import { Box, Grid } from '@mui/material';
+import { Box, Toolbar } from '@mui/material';
+import { Navbar, Sidebar } from '../components';
 
-const drawerWidth = 240;
+const drawerWidth = 270;
 
 export const JournalLayout = ({ children }) => {
     return (
         <>
             <Box sx={{ display: 'flex' }}>
                 {/* Navbar drawerWidth*/}
+                <Navbar drawerWidth={ drawerWidth } /> 
+
                 {/* Sidebar drawerWidth*/}
-                
+                <Sidebar drawerWidth={ drawerWidth } />
+
                 <Box component='main'
                      sx={{ flexGrow: 1, p: 3 }}>
-                        {/* Toolbar */}
+                        <Toolbar />
                         { children }
                 </Box>
             </Box>
